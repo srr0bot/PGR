456d008f44697dc54eac5c55f95221402537319e
srr0bot <danielscmo07@gmail.com>
2024-03-11 15:03:34 -0500

    creacion frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,80 @@
+import { Box, Button, Checkbox, Container, Divider, FormControlLabel, FormGroup, Grid, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import imagenReceta from "../src/images/1229080980.png"
+
+function App() {
+  const [seleccion, setSeleccion] = useState({})
+  const [ingrediente, setingrediente] = useState([])
+  const [respuesta, setRespuesta] = useState([]);
+  const [mostrarImagen, setMostrarImagen] = useState(false);
+  const [mostrarTexto, setMostrarTexto] = useState(false);
+
+  useEffect(() => {
+    async function fetchingrediente() {
+      try {
+        const response = await fetch('/ingredients.json')
+        const data = await response.json()
+        setingrediente(data)
+      } catch (error) {
+        console.error('Error', error)
+      }
+    }
+    fetchingrediente()
+  }, [])
+
+  const manejarSeleccion = (nombre) => {
+    setSeleccion((prevSeleccion) => ({
+      ...prevSeleccion,
+      [nombre]: !prevSeleccion[nombre],
+    }));
+  };
+
+  const enviarSeleccion = async () => {
+    const datos = { ingredientesSeleccionados: seleccion };
+    try {
+      const response = await fetch("http://localhost:5000/api/openai", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datos),
+      });
+      const data = await response.json();
+      console.log(data);
+      setRespuesta(data);
+      setMostrarImagen(true);
+      setMostrarTexto(true);
+      if (respuesta.ok) {
+        console.log("Datos enviados correctamente");
+      } else {
+        console.error("Error al enviar datos al servidor");
+      }
+    } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+    }
+  };
+
+  return(
+    <Container sx={{pt: 5}}>
+        {ingrediente.map((ingrediente)=>(
+            <div key={ingrediente.id}>
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox/>} onChange={() => manejarSeleccion(ingrediente.nombre)} label={ingrediente.nombre}></FormControlLabel>
+                </FormGroup>
+            </div>
+        ))}
+      <Button variant="contained" onClick={enviarSeleccion}>Aceptar ingredientes</Button>
+      <Typography variant="h1">{respuesta.titulo}</Typography>
+      {mostrarTexto && <Typography variant="h3">Ingredientes:</Typography>}
+      <Typography variant="body1">{respuesta.ingredientes}</Typography>
+      {mostrarTexto && <Typography variant="h3">Procedimiento:</Typography>}
+      <Typography variant="body1">{respuesta.procedimiento}</Typography>
+      <Box component="section" sx={{ p: 2, border: '3', width: '500px', height: 'auto'}}>
+      {mostrarImagen && (<img src={imagenReceta} style={{ width: "100%", height: "auto" }}/>)} 
+      </Box>
+    </Container>
+  )
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./index.css";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: '#ea80fc',
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
